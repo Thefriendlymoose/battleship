74b4d25e714f20295af3f0c1c62874d82e76a97c
Thefriendlymoose <alamiridavid@gmail.com>
2021-08-19 13:00:23 +0200

    adding game mechanics

--- main.py
+++ main.py
@@ -2,22 +2,9 @@ import boardclass
 import boatclass
 import actionsclass
 
-d = boardclass.Board("david", 5, 10, "boats")
-shot = boardclass.Board("david", 5, 10, "shots")
-d.createBoard()
-shot.createBoard()
-
-
-
 
 boat1 = boatclass.Boat(4, 1, 0, True)
 boat4 = boatclass.Boat(3, 0, 0, False)
-
-d.addBoat(boat1)
-d.printBoard()
-d.addBoat(boat4)
-
-d.printBoard()
 
 shot1 = actionsclass.Actions(1,1,"david")
 shot2 = actionsclass.Actions(3,3,"david")
@@ -26,3 +13,31 @@ shot2.updateShotBoard(d, shot)
 
 d.printBoard()
 shot.printBoard()
+
+#Variables
+
+player1 = ""
+player2 = ""
+board1 = ""
+board2 = ""
+shotBoard1 = ""
+shotBoard2 = ""
+
+boatSizes = [5, 5, 4, 3, 3, 2, 2]
+gridSizes = [10, 20]
+gameSize = ""
+
+#INIT
+
+player1 = input("advise your name: ")
+player2 = input("advise your name: ")
+
+gameSize = input("please advise following game size %r: " %gridSizes)
+
+board1 = boardclass.Board(player1, len(boatSizes), gameSize, "boats")
+board2 = boardclass.Board(player2, len(boatSizes), gameSize, "boats")
+shotBoard1 = boardclass.Board(player1, len(boatSizes), gameSize, "shots")
+shotBoard2 = boardclass.Board(player2, len(boatSizes), gameSize, "shots")
+
+
+    
